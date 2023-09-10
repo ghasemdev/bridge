457b831bc90cfb7fb3dac4099170268e019cb537
Ghasem Shirdel <shirdelghasem79@gmail.com>
2023-09-10 12:53:53 +0330

    Update key.cpp

--- key.cpp
+++ key.cpp
@@ -1,9 +1,10 @@
-#include <iostream>
-#include <string>
+#include "plusaes.hpp"
+#include <bits/stdc++.h>
 
 using namespace std;
+using namespace plusaes;
 
-char \u02aa[] = {-83,107,42,-58,104,-31,86,108,103,65,103,-6,-52,65,-107,-59,49,4,91,37,-124,-47,-4,110,119};
+char \u02aa[25] = {-83,107,42,-58,104,-31,86,108,103,65,103,-6,-52,65,-107,-59,49,4,91,37,-124,-47,-4,110,119};
 
 string \u02ab(int var0, int var1_1, int var2_2)
 {
@@ -11,7 +12,7 @@ string \u02ab(int var0, int var1_1, int var2_2)
     int var7_3 = var0 * 3 + 83;
     int var6_4 = -1;
     char* var3_5 = \u02aa;
-    int var8_6 = var1_1 * 3 + 21;
+    int var8_6 = var1_1 * 3 + 13;
     char var4_7[var8_6];
     var1_1 = var6_4;
     int var5_8 = var7_3;
@@ -37,27 +38,115 @@ string \u02ab(int var0, int var1_1, int var2_2)
     while (true);
 }
 
-string encryptDecrypt(string toEncrypt)
+int hex_value(unsigned char hex_digit)
 {
-    string key = \u02ab(0, 0, 0);
-    string output = toEncrypt;
+    static const signed char hex_values[256] =
+    {
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            0,  1,  2,  3,  4,  5,  6,  7,  8,  9, -1, -1, -1, -1, -1, -1,
+            -1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        };
+    int value = hex_values[hex_digit];
+    if (value == -1) throw invalid_argument("invalid hex digit");
+    return value;
+}
 
-    for (int i = 0; i < toEncrypt.size(); i++)
-        output[i] = toEncrypt[i] ^ key[i % (sizeof(key) / sizeof(char))];
+string string_to_hex(const string& input)
+{
+    static const char hex_digits[] = "0123456789ABCDEF";
 
+    string output;
+    output.reserve(input.length() * 2);
+    for (unsigned char c : input)
+    {
+        output.push_back(hex_digits[c >> 4]);
+        output.push_back(hex_digits[c & 15]);
+        output.push_back(' ');
+    }
     return output;
+}
+
+string hex_to_string(const string& input)
+{
+    const auto len = input.length();
+
+    string output;
+    output.reserve(len / 2);
+    for (auto it = input.begin(); it != input.end(); )
+    {
+        if(*it == ' ')
+        {
+            *it++;
+        }
+        else
+        {
+            int hi = hex_value(*it++);
+            int lo = hex_value(*it++);
+            output.push_back(hi << 4 | lo);
+        }
+    }
+    return output;
+}
+
+string to_string(const vector<unsigned char>& input)
+{
+    return  string(input.begin(), input.end());
+}
+
+
+vector<unsigned char> to_vector(string const& str)
+{
+    // don't forget the trailing 0...
+    return vector<unsigned char>(str.data(), str.data() + str.length() + 1);
 }
 
 int main()
 {
-    string key = \u02ab(0, 0, 0);
-    cout << "Key: " << key << endl << endl;
+    // AES-GCM 128-bit
 
-    string encrypted = encryptDecrypt("ghasem");
-    cout << "Encrypted:" << encrypted << "\n";
+    const string plain_text = "Hello World!";
 
-    string decrypted = encryptDecrypt(encrypted);
-    cout << "Decrypted:" << decrypted << "\n";
+    vector<unsigned char> key = key_from_string(&"0123456789012345");
 
-    return 0;
+    const unsigned char iv[12] =
+    {
+        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+        0x08, 0x09, 0x0A, 0x0B
+    };
+
+    unsigned char tag[16] =
+    {
+        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+        0x08, 0x09, 0x0A, 0x0B, 0x00, 0x00, 0x00, 0x01
+    };
+
+    cout << "PlainText: " << plain_text << endl << endl;
+    cout << "key: " << to_string(key) << endl << endl;
+
+    // Encrypt ------------------------------------------------------------------------------------------------------------------------------
+
+    encrypt_gcm((unsigned char*)plain_text.data(), plain_text.size(), 0, 0, &key[0], key.size(), &iv, &tag);
+
+    cout << "CipherText: " << plain_text << endl << endl;
+    cout << "CipherText HEX: " << string_to_hex(plain_text) << endl << endl;
+
+    // Decrypt -----------------------------------------------------------------------------------------------------------------------------
+
+    string cipher_text = hex_to_string("E8 91 70 B4 54 3A B9 C6 CF 92 C8 7E");
+
+    decrypt_gcm((unsigned char*)cipher_text.data(), cipher_text.size(), 0, 0, &key[0], key.size(), &iv, &tag);
+    cout << "PlainText: " << cipher_text << endl << endl;
 }
